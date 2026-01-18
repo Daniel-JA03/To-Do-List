d9af42a8515d06d4a02a0ec5a0939f577fe6330d
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-01-18 17:36:35 -0500

    feat: add IUsuario

---
+++ ToDoAPI/Repository/Interfaces/IUsuario.cs
@@ -0,0 +1,12 @@
+﻿using ToDoAPI.Models;
+
+namespace ToDoAPI.Repository.Interfaces
+{
+    public interface IUsuario
+    {
+        string verificarLogin(string correo, string password);
+        string registrarUsuario(Usuario usuario);
+        string obtenerIdUsuario(string correo);
+        bool existeCorreo(string correo);
+    }
+}
