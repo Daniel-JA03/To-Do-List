044b0db218c9ceda5a7a0dbde2d189c4966898b3
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-01-18 17:46:56 -0500

    feat: add ITarea

---
+++ ToDoAPI/Repository/Interfaces/ITarea.cs
@@ -0,0 +1,13 @@
+﻿using ToDoAPI.Models;
+
+namespace ToDoAPI.Repository.Interfaces
+{
+    public interface ITarea
+    {
+        IEnumerable<Tarea> listarTareasPorUsuario(long usuarioId);
+        Tarea obtenerTareaPorId(long id);
+        string agregarTarea(Tarea tarea);
+        string actualizarTarea(Tarea tarea);
+        string eliminarTarea(long id);
+    }
+}
